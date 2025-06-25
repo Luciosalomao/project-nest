ea673576c6fa6331bd1229b52592c182051e398a
Lucio Marcos Salomão de Souza <lucio.souza@el.com.br>
2025-06-25 11:37:25 -0300

    Novos endpoints

--- src/aluno/aluno.controller.ts
+++ src/aluno/aluno.controller.ts
@@ -1,7 +1,8 @@
 import { AlunoService } from './aluno.service';
 import { Aluno } from './aluno.entity';
-import { Controller, Get, Param } from '@nestjs/common';
-
+import { Controller, Get, Post, Delete, Put, Param, Body } from '@nestjs/common';
+import { CreateAlunoDto } from './dto/create-aluno.dto';
+import { UpdateAlunoDto } from './dto/update-aluno.dto';
 
 @Controller('alunos')
 export class AlunoController {
@@ -15,6 +16,24 @@ export class AlunoController {
   @Get('aluno/:id')
   async buscarPorId(@Param('id') id: string): Promise<Aluno> {
     return this.alunoService.buscarPorId(Number(id));
+  }
+
+  @Post('criar')
+  async criar(@Body() createAlunoDto: CreateAlunoDto): Promise<Aluno> {
+    return this.alunoService.criar(createAlunoDto);
+  }
+
+  @Delete('remover/:id')
+  async remover(@Param('id') id: string): Promise<void> {
+    return this.alunoService.remover(Number(id));
+  }
+
+  @Put('atualizar/:id')
+  async atualizar(
+    @Param('id') id: string,
+    @Body() updateAlunoDto: UpdateAlunoDto,
+  ): Promise<Aluno> {
+    return this.alunoService.atualizar(Number(id), updateAlunoDto);
   }
 
 }
--- src/aluno/aluno.service.ts
+++ src/aluno/aluno.service.ts
@@ -3,7 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Aluno } from './aluno.entity';
 import { NotFoundException } from '@nestjs/common';
-
+import { CreateAlunoDto } from './dto/create-aluno.dto';
+import { UpdateAlunoDto } from './dto/update-aluno.dto';
 
 @Injectable()
 export class AlunoService {
@@ -24,6 +25,32 @@ export class AlunoService {
     }
 
     return aluno;
+  }
+
+  async criar(createAlunoDto: CreateAlunoDto): Promise<Aluno> {
+    const novoAluno = this.alunoRepository.create(createAlunoDto);
+    return this.alunoRepository.save(novoAluno);
+  }
+
+  async remover(id: number): Promise<void> {
+    const aluno = await this.alunoRepository.findOne({ where: { id } });
+
+    if (!aluno) {
+      throw new NotFoundException(`Aluno com ID ${id} não encontrado.`);
+    }
+
+    await this.alunoRepository.remove(aluno);
+  }
+
+  async atualizar(id: number, updateAlunoDto: UpdateAlunoDto): Promise<Aluno> {
+    const aluno = await this.alunoRepository.findOne({ where: { id } });
+
+    if (!aluno) {
+      throw new NotFoundException(`Aluno com ID ${id} não encontrado.`);
+    }
+
+    const alunoAtualizado = Object.assign(aluno, updateAlunoDto);
+    return this.alunoRepository.save(alunoAtualizado);
   }
 
 }
